28ecf3787937ffcb3148c50eae9ae231beae459e
Anna Bieliavtseva <belyavtseva1911@gmail.com>
2024-12-24 22:37:12 +0100

    add styles for tablet devices

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -12,14 +12,14 @@ export default function createMarkup(arr) {
       }) =>
         `<li class="photo-card">
           <a href="${largeImageURL}" class="link">
-<img src="${webformatURL}" alt="${tags}" height="200" width="300"/></a>
+          <img src="${webformatURL}" alt="${tags}" height="200" width="300"/></a>
           <div class="info">
           <p class="info-item">Likes: <b>${likes}</b></p>
           <p class="info-item">Views: <b>${views}</b></p>
           <p class="info-item">Comments: <b>${comments}</b></p>
-          <p class="info-item">Downloads:<b> ${downloads}</b></p>
+          <p class="info-item">Downloads:<b>${downloads}</b></p>
           </div>
-
+       
         </li>`
     )
     .join('');
